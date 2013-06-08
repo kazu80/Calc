61963db89c59e1d4fa1bea93b2e337718d28851c
kazuyoshi.kawakami <saruyama.monki@gmail.com>
2013-06-08 21:54:13 +0900

    chapter 3 complete
    教材の簡単計算アプリ完成

--- ProgCalc/ViewController.h
+++ ProgCalc/ViewController.h
@@ -9,5 +9,31 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    int mode;
+}
+
+@property (retain, nonatomic) IBOutlet UILabel *label1;
+@property (retain, nonatomic) IBOutlet UILabel *labelOp;
+@property (retain, nonatomic) IBOutlet UILabel *label2;
+- (IBAction)click7:(id)sender;
+- (IBAction)click8:(id)sender;
+- (IBAction)click9:(id)sender;
+- (IBAction)click4:(id)sender;
+- (IBAction)click5:(id)sender;
+- (IBAction)click6:(id)sender;
+- (IBAction)click1:(id)sender;
+- (IBAction)click2:(id)sender;
+- (IBAction)click3:(id)sender;
+- (IBAction)click0:(id)sender;
+- (IBAction)clickClear:(id)sender;
+- (IBAction)clickOpPlus:(id)sender;
+- (IBAction)clickOpMuins:(id)sender;
+- (IBAction)clickOpMul:(id)sender;
+- (IBAction)clickOpDiv:(id)sender;
+- (IBAction)clickOpEqua:(id)sender;
+
+- (void)pushNum:(int)num;
+- (void)pushOp:(int)op;
 
 @end
